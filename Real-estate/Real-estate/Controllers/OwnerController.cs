3922a09e27f48469661b890e9ae6cd54e1966d5e
KushaniDias <kdias777@gmail.com>
2020-10-16 20:44:23 +0800

    Add OwnerController

---
+++ Real-estate/Real-estate/Controllers/OwnerController.cs
@@ -0,0 +1,20 @@
+﻿using Real_estate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Real_estate.Controllers
+{
+    public class OwnerController : Controller
+    {
+        // GET: Owner
+        private RealEstateContext rec = new RealEstateContext();
+        public ActionResult Index()
+        {
+            List<Owner> AllOwners = rec.Owners.ToList();
+            return View();
+        }
+    }
+}
